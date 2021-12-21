7e9bdff8520e9606fe5644d2c183d8b89ebbaa4f
joao.ferreira@bitcliq.com <joao.ferreira@bitcliq.com>
2021-12-21 13:05:50 +0000

    Check if our substring contains "bitcliq"

--- Start.py
+++ Start.py
@@ -11,11 +11,18 @@ def getRequest(url):
 def getSubstring(string, start, end):
     return string[start:end].strip()
 
+
+#Check if string contains a substring
+def contains(string, substring):
+    return string.find(substring) != -1
+
+
 #Main init function for the program.
 def main():
     print("Test Request!")
     result = getRequest(url="http://www.bitcliq.com") #Call getRequest function
     print(getSubstring(result, result.find("<title>")+7, result.find("</title>"))) #Call getSubstring function
+    print(contains(result, "Bitcliq")) #Call contains function
 
 
 if __name__ == "__main__":
