e7a3e88264abbc588de8ac1084651404c3037b70
joao.ferreira@bitcliq.com <joao.ferreira@bitcliq.com>
2021-12-21 18:39:03 +0000

    Added test results to txt file

--- Start-Test.py
+++ Start-Test.py
@@ -1,24 +1,32 @@
 import Start
 
+ResultFile = 'test-results'
+
+#Save to file
+def SaveToFile(Result):
+    file = open(ResultFile, "w")
+    file.write(Result + "\n")
+    file.close()
+    print(Result)
 
 #test method for getRequest()
 def TestGetRequest(url):
     result = Start.getRequest(url)
     assert result and not result.isspace()
-    print("TestGetRequest() OK!")
+    SaveToFile("TestGetRequest() OK!")
 
 #test method for getSubstring()
 def TestGetSubstring():
     str = "String to test"
     result = Start.getSubstring(str, str.find("String")+len("String"), str.find("test"))
     assert result.strip() == "to"
-    print("TestGetSubstring() OK!")
+    SaveToFile("TestGetSubstring() OK!")
 
 #test method for contains()
 def TestContains():
     str = "321Bitcliq123"
     assert Start.contains(str, "Bitcliq")
-    print("TestContains() OK!")
+    SaveToFile("TestContains() OK!")
 
 def RunTests():
     TestGetRequest("http://www.bitcliq.com")
